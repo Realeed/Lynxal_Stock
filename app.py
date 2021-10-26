8654543dbeb18573ee070065bdc736a46d89e8d4
Realeed <hsargsyan395@gmail.com>
2021-10-26 18:43:04 +0400

    m

--- app.py
+++ app.py
@@ -272,4 +272,4 @@ def searchByFile():
     return redirect(url_for('underDev'))
 
 if __name__  == "__main__":
-    app.run(host='0.0.0.0', port=80, debug=True)
+    app.run(host='0.0.0.0', port=80)
